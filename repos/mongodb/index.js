f401de60026ee29420b521f0aff10c077a31da8a
Shrinath Balakrishnan <shrinath@theredpandas.com>
2017-12-15 18:43:23 +0530

    adding update repo function - untested

--- repos/mongodb/index.js
+++ repos/mongodb/index.js
@@ -1,5 +1,19 @@
+var mongodb = require('mongodb');
+
 module.exports = {
-    withId: (db, model, _id, cb) => db[model].findOne({ _id }, cb),
-    listAll: (db, model, cb) => db[model].find(cb),
-    create: (db, model, obj, cb) => db[model].save(obj, cb)
+    withId:
+        (db, model, _id, cb) => db[model].findOne({ _id }, cb),
+    listAll:
+        (db, model, cb) => db[model].find(cb),
+    create:
+        (db, model, obj, cb) => db[model].save(obj, cb),
+    delete:
+        (db, model, _id, cb) =>
+            db[model].remove({ _id }, { justOne: true }, cb),
+    update:
+        (db, model, _id, attr, cb) =>
+            db[model].findAndModify({
+                query: { _id: obj._id },
+                update: { $set: attr }
+            }, cb)
 }
