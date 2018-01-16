dec53bdf08837b02278778e54807b9f9251e2348
Shrinath Balakrishnan <nitarsh@nitarsh-desktop>
2018-01-16 17:30:03 +0530

    timestamp created

--- repos/mongodb/index.js
+++ repos/mongodb/index.js
@@ -1,19 +1,27 @@
-
+const timestampedObject = obj => {
+    t = Date.now()
+    obj.created = t
+    obj.updated = t
+    return t
+}
 
 module.exports = {
-    withId:
-        (db, model, _id, cb) => db[model].findOne({ _id }, cb),
-    listAll:
-        (db, model, cb) => db[model].find(cb),
-    create:
-        (db, model, obj, cb) => db[model].save(obj, cb),
-    delete:
-        (db, model, _id, cb) =>
-            db[model].remove({ _id }, { justOne: true }, cb),
-    update:
-        (db, model, _id, attr, cb) =>
-            db[model].findAndModify({
-                query: { _id: obj._id },
-                update: { $set: attr }
-            }, cb)
+    withId: (db, model, _id, cb) => db[model].findOne({
+        _id
+    }, cb),
+    listAll: (db, model, cb) => db[model].find(cb),
+    create: (db, model, obj, cb) => db[model].save(timestampedObject(obj), cb),
+    delete: (db, model, _id, cb) => db[model].remove({
+        _id
+    }, {
+        justOne: true
+    }, cb),
+    update: (db, model, _id, attr, cb) => db[model].findAndModify({
+        query: {
+            _id: obj._id
+        },
+        update: {
+            $set: attr
+        }
+    }, cb)
 }
